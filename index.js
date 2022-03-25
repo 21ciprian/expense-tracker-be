287023fe606ba6b22524f2849196d9740854fb88
21ciprian <2021ciprianoffice@gmail.com>
2022-03-25 10:16:14 +0000

    Added port for localhost

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 connectDB()
 app.use(cors())
 app.use(express.json())
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 app.use('/api/v1/transactions', transactionsRouter)
 
 if (process.env.NODE_ENV === 'development') {
