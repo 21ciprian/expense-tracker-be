817d29c5bfb6f80151d3ab9f95114b2c07e0bd25
21ciprian <2021ciprianoffice@gmail.com>
2022-03-25 15:44:34 +0000

    Reversed port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 connectDB()
 app.use(cors())
 app.use(express.json())
-// const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 app.use('/api/v1/transactions', transactionsRouter)
 app.get('/', async function (req, res) {
 	res.send('testing to see if it works')
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'))
 }
 app.listen(
-	process.env.PORT || 5000,
+	PORT,
 	console.log(
 		colors.yellow.bold(
 			`server is listening in ${process.env.NODE_ENV}  on port ${PORT}`
